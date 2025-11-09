7b154db0646f96d5f0ba0ecdc704737ee1b7a232
Josh <jimmyjosh001@gmail.com>
2025-11-09 23:02:54 +0100

    Created Header, MainContent, and Footer components and assembled them in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,21 @@ import WelcomeMessage from './components/WelcomeMessage'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
 
 
 function App() {
