ade576914aadf267a69055a6dbe144942804964d
Josh <jimmyjosh001@gmail.com>
2025-11-23 21:49:52 +0100

    Two folders added to src

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -5,9 +5,7 @@ function App() {
     <div style={{ padding: "20px" }}>
       <h1>GitHub User Search</h1>
       <p>App setup successful. Ready for development!</p>
-      <p>
-        Start by creating components and integrating GitHub's API to search for users.
-      </p>
+     
     </div>
   );
 }
