82bbb3e2ae24c9b5b4fb5934262c65fd22731154
Josh <jimmyjosh001@gmail.com>
2025-11-23 10:14:44 +0100

    still on the counter file

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -35,3 +35,16 @@ function App() {
 }
 
 export default App
+
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <h1>Simple Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
