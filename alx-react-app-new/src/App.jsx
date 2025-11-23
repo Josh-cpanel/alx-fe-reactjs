adc0a1236f0aacb8071d9fee2db0d4942d955901
Josh <jimmyjosh001@gmail.com>
2025-11-23 09:46:08 +0100

    create a counter.jsx file and imported a code that creates an incremental and decremental counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,6 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <h1>Simple Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
+
 
 function App() {
   const [count, setCount] = useState(0)
